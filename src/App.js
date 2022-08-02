d7a5d7f6e56325904c4b3187a7bc901a9f9ba220
github.arief <78076941+arief-github@users.noreply.github.com>
2022-08-02 17:07:56 +0700

    Implementing before spread operator

--- src/App.js
+++ src/App.js
@@ -3,72 +3,78 @@ import React, { useState } from 'react';
 
 // parent const App yang menghandle state
 const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+    const stories = [{
+            title: 'React',
+            url: 'https://reactjs.org/',
+            author: 'Jordan Walke',
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
 
-  const [searchTerm, setSearchTerm] = useState('React');
+    const [searchTerm, setSearchTerm] = useState('React');
 
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-  };
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
 
-  const searchedStories = stories.filter((story) =>
-    story.title.includes(searchTerm)
-  );
+    const searchedStories = stories.filter((story) =>
+        story.title.includes(searchTerm)
+    );
 
-  return (
-    <div>
+    return (
+        <div>
       <h1> My Hackrz Stories </h1>
       <Search onSearch={handleSearch} search={searchTerm} />
       <hr />
       <List list={searchedStories} />
     </div>
-  );
+    );
 };
 
 
 // method list, search, item hanya passing props dan return view
+const Search = ({ onSearch, search }) => {
+    return (
+        <div>
+      <label htmlFor="search"> Search : </label>
+      <input id="search" type="text" onChange={onSearch} value={search} />
+    </div>
+    );
+};
+
 const List = ({ list }) => (
-  <ul>
+    <ul>
     {list.map((item) => (
-      <Item key={item.objectID} item={item} />
+      <Item 
+        key={item.objectID} 
+        title={item.title}
+        url={item.url}
+        author={item.author}
+        num_comments = {item.num_comments}
+        points={item.points}
+      />
     ))}
   </ul>
 );
 
-const Search = ({ onSearch, search }) => {
-  return (
-    <div>
-      <label htmlFor="search"> Search : </label>
-      <input id="search" type="text" onChange={onSearch} value={search} />
-    </div>
-  );
-};
-
-const Item = ({ item }) => (
-  <li>
+const Item = ({ title, url, author, num_comments, points }) => (
+    <li>
     <span>
-      <a href={item.url}> {item.title} </a>
+      <a href={url}> {title} </a>
     </span>
-    <span> {item.author} </span>
-    <span> {item.num_comments} </span>
-    <span> {item.points} </span>
+    <span> {author} </span>
+    <span> {num_comments} </span>
+    <span> {points} </span>
   </li>
 );
 
